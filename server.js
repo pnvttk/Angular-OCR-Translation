2bc37e6561fc1b11b0668a05551595afe41dc59b
pnvttk <pannavach@outlook.com>
2022-04-13 16:47:21 +0700

    update package.json

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-app.use(express.static('./dist/angular-ocr-tl'))
+app.use(express.static('./dist/angular-ocr-tl'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/ng-ocr-mtl/'}),
+    res.sendFile('index.html', {root: 'dist/angular-ocr-tl/'}),
 );
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
