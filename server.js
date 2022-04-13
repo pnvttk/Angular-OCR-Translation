af588716452569b37aff9bbf4adc2eb6a8b1ccc4
Pannavach T. <40768385+pnvttk@users.noreply.github.com>
2022-04-13 16:35:14 +0700

    fix server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const expres = require('express')
 const path = require('path')
 
-const app = expres()
+const app = express()
 
 app.use(expres.static('./dist/angular-ocr-tl'))
 
-app.get('/*', (req,res)=>{
-    res.sendFile('index.html', {root: 'dist/angular-ocr-tl/'})
-})
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/ng-ocr-mtl/'}),
+);
 
 app.listen(process.env.PORT || 3000)
