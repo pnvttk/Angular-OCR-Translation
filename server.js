bc9f953f9d2357a7ebc06028514fedaa4d6b8a41
pnvttk <pannavach@outlook.com>
2022-04-13 16:38:23 +0700

    fix server.js2

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const expres = require('express')
+const express = require('express')
 const path = require('path')
 
 const app = express()
 
-app.use(expres.static('./dist/angular-ocr-tl'))
+app.use(express.static('./dist/angular-ocr-tl'))
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/ng-ocr-mtl/'}),
